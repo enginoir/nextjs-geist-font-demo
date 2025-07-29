2ef3378a55cada3545f362c1fcda35a8d135848b
Enginoir <124773538+enginoir@users.noreply.github.com>
2025-07-29 12:07:00 +0700

    Add .next/static/chunks/src_app_page_tsx_0ef0cac3._.js 1753765619428

---
+++ .next/static/chunks/src_app_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_dd3f96bf._.js",
+  "static/chunks/src_35db6916._.js"
+],
+    source: "dynamic"
+});
